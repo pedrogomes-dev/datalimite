afc2491d1afab73f802f786f08ebcffd97470dc1
PG <pedrogomes@detic.ro.gov.br>
2021-01-14 08:35:13 -0400

    teste includes

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Server running on: http://localhost:" + app.get('port'))
 })
